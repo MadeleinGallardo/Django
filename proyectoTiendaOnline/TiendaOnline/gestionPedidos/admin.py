07cb32960e35dba2ee0775e760422c0ed8395249
MadeleinGallardo <madeleinagallardo@gmail.com>
2021-08-02 22:10:44 -0300

    Cree un proyecto nuevo, hice modelos y configure panel de control

---
+++ proyectoTiendaOnline/TiendaOnline/gestionPedidos/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.db import models
+from gestionPedidos.models import Clientes, Articulos, Pedidos
+# Register your models here.
+
+class ClientesAdmin(admin.ModelAdmin):
+    list_display= ('nombre', 'direccion', 'telefono')
+    search_fields = ('nombre', 'telefono')
+
+
+admin.site.register(Clientes, ClientesAdmin)
+admin.site.register(Articulos)
+admin.site.register(Pedidos)
+
+
